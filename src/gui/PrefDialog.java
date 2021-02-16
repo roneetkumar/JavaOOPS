32f4f62c9f7f621aec18181d34ad4198a4cf8153
Roneet Kumar <roneetparida@gmail.com>
2021-02-16 06:22:11 -0500

    added user and password fields

--- src/gui/PrefDialog.java
+++ src/gui/PrefDialog.java
@@ -10,15 +10,21 @@ public class PrefDialog extends JDialog {
     private JButton okButton;
     private JButton cancelButton;
     private JSpinner portSpinner;
-
     private SpinnerNumberModel spinnerModel;
 
+    private JTextField userField;
+    private  JPasswordField passwordField;
 
     public PrefDialog(JFrame parent){
         super(parent,"Settings",false );
 
         okButton = new JButton("OK");
         cancelButton = new JButton("Cancel");
+
+        userField = new JTextField(10);
+        passwordField = new JPasswordField(10);
+        passwordField.setEchoChar('#');
+
 
         spinnerModel = new SpinnerNumberModel(3306,0,9999,1);
 
@@ -28,6 +34,33 @@ public class PrefDialog extends JDialog {
         GridBagConstraints gc = new GridBagConstraints();
 
         gc.gridy = 0;
+
+        // Next Row
+
+        gc.weightx = 1;
+        gc.weighty = 1;
+        gc.fill = GridBagConstraints.NONE;
+        gc.gridx = 0;
+        add(new JLabel("User"),gc);
+
+        gc.gridx++;
+        add(userField,gc);
+
+        // Next Row
+
+        gc.gridy++;
+        gc.weightx = 1;
+        gc.weighty = 1;
+        gc.fill = GridBagConstraints.NONE;
+        gc.gridx = 0;
+        add(new JLabel("Password"),gc);
+
+        gc.gridx++;
+        add(passwordField,gc);
+
+        // Next Row
+        gc.gridy++;
+
         gc.weightx = 1;
         gc.weighty = 1;
         gc.fill = GridBagConstraints.NONE;
@@ -39,7 +72,6 @@ public class PrefDialog extends JDialog {
         add(portSpinner,gc);
 
         // Next Row
-
         gc.gridy++;
         gc.gridx = 0;
         add(okButton,gc);
@@ -51,7 +83,13 @@ public class PrefDialog extends JDialog {
         okButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 Integer value = (Integer)portSpinner.getValue();
-                System.out.println(value);
+
+                String user = userField.getText();
+
+                char[] password =  passwordField.getPassword();
+
+
+                System.out.println(user + ", " +  new String(password));
                 setVisible(false);
             }
         });
